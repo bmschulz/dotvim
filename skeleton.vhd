d9960a3a478a971edc98e9680d0d180294743587
Brandon <bmschulz@users.noreply.github.com>
2017-04-10 13:02:39 -0700

    Convert tabs to spaces in template

--- skeleton.vhd
+++ skeleton.vhd
@@ -1,7 +1,7 @@
 --------------------------------- FILE HEADER -------------------------------{{{
 --
--- File:			[:VIM_EVAL:]expand("%:t")[:END_EVAL:]
--- Last Modified:	06 Jan 2016  04:56PM
+-- File:            [:VIM_EVAL:]expand("%:t")[:END_EVAL:]
+-- Last Modified:   10 Apr 2017  09:45AM
 --
 -- Module Description
 --
@@ -16,15 +16,15 @@ use ieee.std_logic_1164.all;
 
 ------------------------------------ ENTITY ---------------------------------{{{
 entity [:VIM_EVAL:]expand("%:t:r")[:END_EVAL:] is
---	generic (
---		width : integer := 2
---	);
-	port (
-		o_module_date		: out	std_logic_vector(31 downto 0) := x"0106_2016"	;
-		o_module_time		: out	std_logic_vector(31 downto 0) := x"1656_5200"	;
-		i_reset				: in	std_logic										;
-		i_clk				: in	std_logic
-	);
+--  generic (
+--      width : integer := 2
+--  );
+    port (
+        o_module_date    : out std_logic_vector(31 downto 0) := x"0410_2017" ;
+        o_module_time    : out std_logic_vector(31 downto 0) := x"0945_1000" ;
+        i_reset          : in std_logic                                      ;
+        i_clk            : in std_logic
+    );
 end entity [:VIM_EVAL:]expand("%:t:r")[:END_EVAL:];
 -----------------------------------------------------------------------------}}}
 
@@ -34,8 +34,8 @@ architecture behavioral of [:VIM_EVAL:]expand("%:t:r")[:END_EVAL:] is
 
 ------------------------------ SIGNAL DECLARATION ---------------------------{{{
 --type state_type is (IDLE, RUN);
---signal next_state		: state_type := IDLE;
---signal state			: state_type := IDLE;
+--signal next_state     : state_type := IDLE;
+--signal state          : state_type := IDLE;
 -----------------------------------------------------------------------------}}}
 
 begin
